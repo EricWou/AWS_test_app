54352b03e8abe95b10c41bae21a9820b86ee2a4b
Eric Wou <ericwou94@gmail.com>
2024-07-16 12:25:15 -0400

    added user4 and updated node.js.yml

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ const buildPath = path.join(_dirname, "../client/build");
 app.use(express.static(buildPath));
 
 app.get('/api', (req, res) => {
-    res.json({ users: ["user1", "user2", "user3"] });
-})
+    res.json({ users: ["user1", "user2", "user3", "user4"] });
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(_dirname, "../client/build/index.html"), (err) => {
@@ -22,3 +22,8 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 })
+
+//to connect with AWS Ubuntu server, on PowerShell with admin privileges:
+//cd \
+//cd users\ericw\desktop
+//ssh -i "MyConnectionPair.pem" ubuntu@ec2-3-142-46-32.us-east-2.compute.amazonaws.com
